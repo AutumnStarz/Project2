1e8b05cdf0ebfd5ca4e726d3ca335896ca376ed3
AutumnStarz <143517148+AutumnStarz@users.noreply.github.com>
2025-04-17 10:06:21 -0400

    commit

--- server/app.js
+++ server/app.js
@@ -24,6 +24,15 @@ console.log('Looking for views in:', path.join(__dirname, 'views'));
 
 app.use('/hosted', express.static(path.join(__dirname, '..', 'hosted')));
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
